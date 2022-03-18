562c46276fe5c71c62428959e2e7a388622c5e53
Justin Mitchel <j@teamcfe.com>
2022-03-18 14:23:23 -0500

    Removed secret key to env

--- cfeblog/settings.py
+++ cfeblog/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-!t!%!mro$51)_zc3_$1twz9g0mhodium)_gzc7)lkf8x!p6&(6"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = str(os.environ.get("DJANGO_DEBUG")) == "1"
 
 ALLOWED_HOSTS = []
 
